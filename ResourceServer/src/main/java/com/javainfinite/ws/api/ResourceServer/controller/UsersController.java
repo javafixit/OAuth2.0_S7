975adacd0235aca56affa11f4cf8b4c58742328b
javafixit <vikrams.java@gmail.com>
2022-03-22 12:40:17 +0530

    Section_7

---
+++ ResourceServer/src/main/java/com/javainfinite/ws/api/ResourceServer/controller/UsersController.java
@@ -0,0 +1,15 @@
+package com.javainfinite.ws.api.ResourceServer.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UsersController {
+
+    @GetMapping("/status/check")
+    public String getUser() {
+        return "Working...";
+    }
+}
